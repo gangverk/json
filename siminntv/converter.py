c418de14054a2778541873b787628d6470fc3931
Atli Thorbjornsson <atli@gangverk.is>
2013-04-24 11:42:53 +0000

    wrote a converter and renaming to fit existing structure

---
+++ siminntv/converter.py
@@ -0,0 +1,40 @@
+import json
+import sys
+
+if len(sys.argv) == 1:
+	print "You need to at least provide a source file, optional width and height of source screen"
+	exit()
+
+source = sys.argv[1]
+
+origin_width = 1070
+if len(sys.argv) > 2:
+	origin_width = sys.argv[2]
+origin_height = 544
+if len(sys.argv) > 3:
+	origin_width = sys.argv[3]
+
+
+xparams = set(["width","x","horizontal-spacing"])
+yparams = set(["height","y","vertical-spacing"])
+
+def loop(itemlist):
+	for v in itemlist:
+		if isinstance(v,dict):
+			traverse(v)
+
+def traverse(node):
+	for k,v in node.iteritems():
+		if k in yparams:
+			node[k] = float(v)/float(origin_height)
+		elif k in xparams:
+			node[k] = float(v)/float(origin_width)
+		elif isinstance(v,dict):
+			traverse(v)
+		elif isinstance(v,list):
+			loop(v)
+
+
+d = json.loads(open(source).read())
+traverse(d)
+print json.dumps(d, sort_keys=True,indent=4, separators=(',', ': '))
