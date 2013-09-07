6739562602a20041567a61cb39621b2ef6c78716
Atli Thorbjornsson <atli@gangverk.is>
2013-09-07 19:57:09 +0000

    jenkins

--- siminntv/converter.py
+++ siminntv/converter.py
@@ -2,30 +2,31 @@ import json
 import sys
 
 if len(sys.argv) == 1:
-	print "You need to at least provide a source file, optional width and height of source screen"
+	print "You need to at least provide a source file and optional height of source screen"
 	exit()
 
 source = sys.argv[1]
 
 origin_height = 544
 if len(sys.argv) > 2:
-	origin_width = sys.argv[2]
+	print "setting height to ", sys.argv[2]
+	origin_height = int(sys.argv[2])
 
 
-xparams = set(["width","x","horizontal-spacing"])
-yparams = set(["height","y","vertical-spacing"])
+
+params = set(["width","height","x","y","horizontal-spacing","vertical-spacing"])
 
 def loop(itemlist):
 	for v in itemlist:
 		if isinstance(v,dict):
 			traverse(v)
+		elif isinstance(v,list):
+			loop(v)
 
 def traverse(node):
 	for k,v in node.iteritems():
-		if k in yparams:
-			node[k] = float(v)/float(origin_height)
-		elif k in xparams:
-			node[k] = float(v)/float(origin_height)
+		if k in params:
+			node[k] = round(float(v)/float(origin_height),3)
 		elif isinstance(v,dict):
 			traverse(v)
 		elif isinstance(v,list):
@@ -33,5 +34,12 @@ def traverse(node):
 
 
 d = json.loads(open(source).read())
-traverse(d)
+if isinstance(d,dict):
+	traverse(d)
+elif isinstance(d,list):
+	loop(d)
+else:
+	print "What the hell is this:",type(d)
+
+
 print json.dumps(d, sort_keys=True,indent=4, separators=(',', ': '))
